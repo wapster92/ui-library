478771e29bcd20c155e404b3902f58a366247ccf
wapster <tursunbaev@indins.ru>
2022-03-28 12:14:01 +0300

    Обновил конфигурацию для ts и добавил поддержку alias в редакторы

--- src/vue.d.ts
+++ src/vue.d.ts
@@ -1,10 +1,5 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  VITE_APP_TITLE: string;
-  VITE_API_URL: string;
-}
-
 declare module '*.vue' {
   import { DefineComponent } from 'vue';
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
